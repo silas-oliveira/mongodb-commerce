2a951621c00d19d0ffa3a414296751882c220028
Armin1-lab <silas.oliveira8@gmail.com>
2022-04-06 00:01:27 -0300

    req 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,5 @@
+// retorne: nome e curtidas 
+// condição: curtidas maiores que 10 e menores que 100
+
+db.produtos.find({ $and: [{ curtidas: { $gt: 10, $lt: 100 } }] },
+ { _id: 0, nome: 1, curtidas: 1 });
